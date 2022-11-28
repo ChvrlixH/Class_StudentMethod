2d9ccb13066f9a9218648a088ab72ca1794ce6cf
ChvrlixH <ehedzade.huseyn@gmail.com>
2022-11-29 01:29:22 +0400

    Students (Exam Results)

---
+++ Class_Method/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Class_Method
+{
+    internal class DataBase
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduated;
+
+        public DataBase(string name, string surname, string group, int point, bool isgraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = isgraduated;
+
+        }
+
+        public void Graduated()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("Tebrik edirik"+" "+ Name + " " + Surname + " " +"artiq mezunsunuz!");
+                Console.Write("Baliniz: ");
+            }
+            else
+            {
+                Console.WriteLine("Teessuf ki"+" "+ Name + " " + Surname + " " +"mezun ola bilmediniz...");
+                Console.Write("Baliniz: ");
+            }
+        }
+
+        public void Points()
+        {
+            if (Point > 80)
+            {
+                Console.WriteLine("2-ci defe yeniden imtahan vermek haqqini qazandiniz.");
+            }
+            
+        }
+
+    }
+}
---
+++ Class_Method/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Class_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mezun olmaq ucun minumum teleb olunan bal: 100/61");
+            Console.WriteLine("(Isteye bagli) 2-ci imtahana girmek ucun minumum teleb olunan bal: 100/81");
+            Console.WriteLine("");
+            Console.WriteLine("Imtahan neticesi:");
+            DataBase Student1 = new DataBase("Huseyn", "Ahadzada", "P327", 77, true);
+            DataBase Student2 = new DataBase("Ayxan", "Memmedov", "P327", 89, true);
+            DataBase Student3 = new DataBase("Sahin", "Rehmanov", "P327", 85, true);
+            DataBase Student4 = new DataBase("Vusal", "Ibrahimov", "P405", 55, false);
+            DataBase Student5 = new DataBase("Resid", "Suleymanzade", "P255", 43, false);
+            DataBase Student6 = new DataBase("Fikret", "Emiraslanov", "P327", 100, true);
+            DataBase Student7 = new DataBase("Saleh", "Imanov", "P132", 65, true);
+            Student2.Graduated();
+            Console.WriteLine(Student2.Point);
+            Student2.Points();
+
+        }
+    }
+}
